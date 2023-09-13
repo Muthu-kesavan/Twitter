3cf2efc3eef5ea8f15656c4133cd86f67e6821c5
Muthu kesavan S <muthukesavan6044@gmail.com>
2023-09-13 14:23:08 +0530

    Day 1 - Commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auths.js";
 const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+const connect = () => {
+    mongoose.set("strictQuery", false);
+    mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        throw err;
+    });
+};
 
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 app.listen(8000, () => {
+    connect();
     console.log("Listening to port 8000")
 });
