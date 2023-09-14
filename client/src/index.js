4a816013d32cce2fcb9b69b755c5f44017e6e4e8
Muthu kesavan S <muthukesavan6044@gmail.com>
2023-09-14 22:25:55 +0530

    Day 2 - Commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+import { persistGate } from "redux-persist/integration/react";
+import { persistor } from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <persistGate loading={null} persistor={persistor}>
     <App />
+    </persistGate>
+    </Provider>
   </React.StrictMode>
 );
 
