d87b42a3d32f166a0e30634ca285ec1415855a20
Muthu kesavan S <muthukesavan6044@gmail.com>
2023-09-14 16:44:39 +0530

    Day 2- Commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import SearchIcon from "@mui/icons-material/Search";
+import StarBorderIcon from "@mui/icons-material/StarBorderPurple500";
 const Navbar = () => {
   return (
     <div className="grid grid-col-1 md:grid-cols-4 my-5 justify-center">
@@ -13,10 +14,11 @@ const Navbar = () => {
         <div className="col-span-2 md:border-x-2 md:border-slate-200 md:px-6 my-6 md:my-0">
             <div className="flex justify-between items-center">
             <h2 className="font-bold text-2xl">Home</h2>
+            <StarBorderIcon />
             </div>
             </div> 
             <div className="px-0 md:px-6 mx-auto">
-                <p>search logo</p>
+                <SearchIcon className="absolute m-2"/>
                 <input type="text" className=" bg-blue-100 rounded-full py-2 px-8"/>
             </div>
         </div>
